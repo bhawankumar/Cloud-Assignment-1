7e9cc5868ab4435545b5afdbdbcfd48e1b560fde
bhawankumar <77658206+bhawankumar@users.noreply.github.com>
2022-03-14 15:28:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,16 @@ app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.get('*', (req, res) => {
+      res.sendfile(
+        path.join(__dirname = 'client/build/index.html')
+      );
+    }
+    )
+  }
+  
 app.listen(PORT, () => {
     console.log('Server running on localhost:' + PORT);
 });
